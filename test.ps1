9eb992f2406b94bd49d8e58eb9afb7402e1076fc
alain <chadnpc@outlook.com>
2026-03-20 03:14:23 +0200

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,15 @@
+﻿
+# .SYNOPSIS
+# Tests the script
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory = $false, Position = 0)]
+  [Alias('Module')][string]$ModulePath = $PSScriptRoot,
+  # Path Containing Tests
+  [Parameter(Mandatory = $false, Position = 1)]
+  [Alias('Tests')][string]$TestsPath = [IO.Path]::Combine($PSScriptRoot, 'Tests')
+)
+
+$TestResults = Invoke-Pester -OutputFormat NUnitXml -OutputFile ([IO.Path]::Combine("Tests", "results.xml")) -PassThru
+
+# ... do stuff with $TestResults
